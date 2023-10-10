48bc3aacaaebbed8a01205084acda2e66f8f02d8
Guilherme Oliveira <guilherme77oliveira@gmail.com>
2023-10-10 13:20:57 -0300

    Delete userdept directory

--- userdept/src/main/java/com/guiwoliveira/userdept/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.guiwoliveira.userdept.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.guiwoliveira.userdept.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    
-}
--- userdept/target/classes/import.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO tb_department(name) VALUES ('Gestão');
-INSERT INTO tb_department(name) VALUES ('Informática');
-
-INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Guilherme', 'guilherme@gmail.com');
-INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Natalia', 'natalia@gmail.com');
-INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Alex', 'alex@gmail.com');
-INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Rosangela', 'rosangela@gmail.com');
